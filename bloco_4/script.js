6d1c8dbf79a5eb51342989937bcb25f73d0a1acb
Luís Seixas <luiseixas2@gmail.com>
2021-03-24 18:24:27 -0300

    resolucao do quesito 1 - Primeiros Passos Em JS

---
+++ bloco_4/script.js
@@ -0,0 +1,11 @@
+let a = 4;
+let b = 10;
+
+let soma = a + b;
+console.log(soma);
+let subtracao = a - b;
+console.log(subtracao);
+let multiplcacao = a * b;
+console.log(multiplcacao);
+let divisao = a / b;
+console.log(divisao);    
