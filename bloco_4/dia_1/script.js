ce3e2b6f1ed6804a14acbd79b1702ce7790e0ea2
Luís Seixas <luiseixas2@gmail.com>
2021-03-24 15:20:37 -0300

    inciando os estudos em js

---
+++ bloco_4/dia_1/script.js
@@ -0,0 +1,8 @@
+const name = "Luis";
+const birthCity = "São Luís"; 
+const birthYear = 1993;
+
+console.log(name + " " + "nasceu em" + " " + birthCity + " em" + " " + birthYear + ".");
+
+birthYear = 1994;
+console.log(birthYear);
