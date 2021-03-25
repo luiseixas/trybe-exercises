20de153c9e6df7634a5db588cc37a7cd3a958d04
Luís Seixas <luiseixas2@gmail.com>
2021-03-24 21:36:54 -0300

    resolucao do quesito 7 - Primeiros passos em JS

---
+++ bloco_4/dia_1/script7.js
@@ -0,0 +1,18 @@
+let nota = 75;
+
+if(nota < 0 || nota > 100){
+    console.log("ERRO")
+}
+  else if (nota >= 90 && nota <= 100){
+    console.log("A")
+} else if (nota >= 80){
+    console.log("B")
+} else if (nota >= 70){
+    console.log("C")
+} else if (nota >= 60){
+    console.log("D")
+} else if (nota >= 50){
+    console.log("E")
+} else if (nota < 50 && nota >= 0) {
+    console.log("F")
+}
