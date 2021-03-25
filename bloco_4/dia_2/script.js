eca6cdddd9cc1529017115c72cd19a8054f01f80
Luís Seixas <luiseixas2@gmail.com>
2021-03-25 18:38:32 -0300

    quesito 2 - Arrays e Loop For

--- bloco_4/dia_2/script.js
+++ bloco_4/dia_2/script.js
@@ -2,3 +2,8 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 console.log(numbers);
 
+let soma = 0;
+for (i = 0; i < numbers.length; i +=1) {
+    soma = numbers[i] + soma;  
+}
+console.log(soma);
