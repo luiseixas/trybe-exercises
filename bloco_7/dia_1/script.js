9395d922fbd920787382517e39520149b25ffe35
Luís Seixas <luiseixas2@gmail.com>
2021-04-26 20:35:28 -0300

    exercicios do bloco 7 dia 1

---
+++ bloco_7/dia_1/script.js
@@ -0,0 +1,23 @@
+const testingScope = (escopo) => {
+    if (escopo === true) {
+      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+      console.log(ifScope);
+    } else {
+      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+      console.log(elseScope);
+    }
+}
+
+  testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+  
+// Seu código aqui.
+oddsAndEvens.sort(function(a, b) {
+    if(a > b) return 1;
+    if(a < b) return -1;
+    return 0;
+});
+
+console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
