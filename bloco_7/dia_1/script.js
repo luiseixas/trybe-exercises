f29a4144d02d30123def8af8687d8b37096fcdf8
Luís Seixas <luiseixas2@gmail.com>
2021-05-04 20:52:28 -0300

    ajustes

--- bloco_7/dia_1/script.js
+++ bloco_7/dia_1/script.js
@@ -1,3 +1,7 @@
+// Parte I
+
+// Exercício I
+
 const testingScope = (escopo) => {
     if (escopo === true) {
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -9,15 +13,61 @@ const testingScope = (escopo) => {
     }
 }
 
-  testingScope(true);
+testingScope(true);
+
+// Exercício II
 
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
   
 // Seu código aqui.
-oddsAndEvens.sort(function(a, b) {
+oddsAndEvens.sort((a, b) => {
     if(a > b) return 1;
     if(a < b) return -1;
     return 0;
 });
 
 console.log(`Os números ${oddsAndEvens} se encontram ordenados de forma crescente!`);
+
+//parte II
+
+// Exercício I
+
+const calcFactorial = (num) => {
+  if(num === 0 || num === 1) {
+    return 1;
+  //recursive case
+  } else {
+    return num * calcFactorial(num - 1);
+  }
+}
+let num = 4;
+console.log(`O fatorial de ${num} é: ${calcFactorial(num)}`);
+
+// Exercício II 
+
+function longestWord(phrase) {
+
+  let words = phrase.split(' ');
+  let sizeWord = 0;
+  let result = '';
+  
+  for (let index of words) {
+    if(index.length > sizeWord) {
+      sizeWord = index.length;
+      result = index;
+    } 
+  } 
+  return result;
+}
+console.log(longestWord('Minha terra tem palmeiras onde canta o sabiá'));
+
+// Exercício III
+
+let clickCount = 0;
+let textToAppear = document.getElementById('text');
+const buttonClick = document.getElementById('btn-1');
+
+buttonClick.addEventListener('click', () => {
+  textToAppear.innerHTML = clickCount+=1;
+});
+
