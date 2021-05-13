7947dce4a30191f20fea222ab2849f76bad3228d
Luís Seixas <luiseixas2@gmail.com>
2021-05-13 16:38:22 -0300

    exercicios dia 1

--- bloco_8/dia_1/script.js
+++ bloco_8/dia_1/script.js
@@ -1,3 +1,5 @@
+//Exercício I
+
 const employeeGenerator = (fullName) => {
     const email = fullName.toLowerCase().split(' ').join('_');
     return { fullName, email: `${email}@trybe.com` };
@@ -13,3 +15,23 @@ const employeeGenerator = (fullName) => {
   };
   
   console.log(newEmployees(employeeGenerator));
+
+  //Exercício II
+
+  const generateRandomNumber = () => {
+      
+  }
+
+  //
+
+  const pessoas = [ 
+    { nome: 'Luís', age: 28 },
+    { nome: 'Stephanie', age: 29 },
+    { nome: 'Emanuela', age: 21},
+    { nome: 'Francineide', age: 46}
+  ]
+
+
+  const names = pessoas.map(value => value.nome );
+
+  console.log(names);
