c159ab53e499e395a6c71bbd4357ee3ac48afea9
Luís Seixas <luiseixas2@gmail.com>
2021-05-24 16:51:17 -0300

    exrcicio dia_2

---
+++ bloco_9/dia_2/script.js
@@ -0,0 +1,8 @@
+const apiUrl = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
+const listItens = document.querySelector('#itens-list');
+const itens = [{id:}
+
+
+
+fetch(apiUrl)
+  .then()
