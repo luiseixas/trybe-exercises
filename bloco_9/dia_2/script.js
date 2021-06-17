e3e7793e85eeba4773f722b5d3a5da0af7b3b548
Luís Seixas <76114544+luiseixas@users.noreply.github.com>
2021-06-17 17:29:55 -0300

    Delete script.js

--- bloco_9/dia_2/script.js
+++
@@ -1,8 +0,0 @@
-const apiUrl = 'https://api.mercadolibre.com/sites/MLB/search?q=$QUERY';
-const listItens = document.querySelector('#itens-list');
-const itens = [{id:}
-
-
-
-fetch(apiUrl)
-  .then()
